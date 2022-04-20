ac21969eafe726b7c90d80555a40d9d8e43b1029
Ricardo Nunes <riic.nun@gmail.com>
2022-04-20 12:57:47 -0300

    desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,38 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if(!newTaskTitle) {
+      return
+    }
+
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, task])
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   }
 
   function handleToggleTaskCompletion(id: number) {
+
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task)
+
+    setTasks(task)
+  
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const task = tasks.filter(task => task.id != id)
+
+    setTasks(task)
   }
 
   return (
